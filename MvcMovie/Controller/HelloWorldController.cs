aa1abfee2a63756f9103636d57002b466aeff2e2
Znamenskas <zvilandas@gmail.com>
2022-07-25 10:15:03 +0300

    Sikurtas naujas projektas

---
+++ MvcMovie/Controller/HelloWorldController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class PrekesController : Controller
+    {
+        // 
+        // GET: /Prekes/
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string PavyzdinePreke()
+        {
+            return "Laimekite nauja televizoriu...";
+        }
+        // GET: /HelloWorld/Welcome/ 
+        // Requires using System.Text.Encodings.Web;
+        public string RodytiPrekes(string pavadinimas, decimal kaina, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Prekes: {pavadinimas}, Kaina: { kaina}, ID: {ID}");
+        }
+    }
+}
